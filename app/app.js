75a0e10135c5a81c601a85425a18382ada75e236
avongalie <alexsiaavila@gmail.com>
2022-04-16 12:43:14 -0400

    set up sign in sign out functionality

--- app/app.js
+++ app/app.js
@@ -1,9 +1,25 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+
+const authEvents = require('./auth/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#logged-in').hide();
+  
+  $('#sign-up-form').on("submit", authEvents.onSignUp);
+  $("#log-in-form").on("submit", authEvents.onSignIn);
+  $('#sign-out').hide();
+ $('#log-in-button').on('click', function(){
+   $('.display-text').text('')
+  $('.modal-backdrop').show();
+ })
+ $('#sign-up-button').on('click',function() {
+  $('#sign-up-form').show();
+   $('.display-text').text('')
+  })
+
+  $('#sign-out').on('click', authEvents.onSignOut);
 })
---
+++ app/auth/ui.js
@@ -0,0 +1,61 @@
+//file that deals with what the user sees
+
+const store = require('../store.js');
+const authEvents = require('./events.js');
+
+const onSignUpSuccess = function(){
+    //$('#sign-up').hide();
+    //$('#sign-in').show();
+    $('form').trigger('reset');
+    $('#sign-up-form').hide();
+    $('.display-text').text('Return to page and log-in');
+    // $('#signUpModal').hide();
+    // $('.modal-backdrop').hide();
+}
+
+const onSignUpFailure = function(data){
+    $('.display-text').text(`Account with ${data.credentials.email} already exists`);
+}
+
+const onSignInSuccess = function(response){
+    console.log(response);
+    $('.display-text').text('');
+    $('form').trigger('reset');
+    $('#sign-in').hide();
+    $('#logged-in').show();
+    //$('#begin-game').show();
+    //$('#welcome').text(`Welcome ${response.user.email}`);
+    $('#logInModal').hide();
+    $('.modal-backdrop').hide();
+     $('#sign-out').show();
+    store.user = response.user;
+
+}
+
+const onSignInFailure = function(){
+    $('.display-text').text('Email or Password is incorrect');
+}
+
+const onSignOut = function(){
+    //return to main page
+    $('#sign-in').show();
+    $('#logged-in').hide();
+     $('#sign-out').hide();
+    
+}
+
+const onChangePasswordSuccess = function(){
+
+}
+
+const onChangePasswordFailure = function(){
+
+}
+
+module.exports = {
+    onSignUpFailure,
+    onSignUpSuccess,
+    onSignInFailure,
+    onSignInSuccess,
+    onSignOut
+}
