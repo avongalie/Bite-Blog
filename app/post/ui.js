b5b8387a1cac2f9a74515b7afef0a7a2916e6a74
avongalie <alexsiaavila@gmail.com>
2022-04-18 09:04:15 -0400

    app/post

---
+++ app/post/ui.js
@@ -0,0 +1,47 @@
+//file that deals with what the user sees
+
+const store = require('../store.js');
+const authEvents = require('./events.js');
+
+const onSignUpSuccess = function(){
+    $('#sign-up').hide();
+    $('#sign-in').show();
+    $('form').trigger('reset');
+    $('#sign-up-form').hide();
+    $('.display-text').text('Return to page and log-in');
+    
+}
+
+const onSignUpFailure = function(data){
+    $('.display-text').text(`Account with ${data.credentials.email} already exists`);
+}
+
+const onSignInSuccess = function(response){
+    console.log(response);
+    $('.display-text').text('');
+    $('form').trigger('reset');
+    $('#sign-in').hide();
+    $('#begin-game').show();
+    $('#welcome').text(`Welcome ${response.user.email}`);
+    store.user = response.user;
+
+}
+
+const onSignInFailure = function(){
+    $('.display-text').text('Email or Password is incorrect');
+}
+
+const onChangePasswordSuccess = function(){
+
+}
+
+const onChangePasswordFailure = function(){
+
+}
+
+module.exports = {
+    onSignUpFailure,
+    onSignUpSuccess,
+    onSignInFailure,
+    onSignInSuccess
+}
